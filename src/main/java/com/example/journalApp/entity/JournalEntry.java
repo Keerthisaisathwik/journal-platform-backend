faaf295c3c6677a30defb29094532d88760d62de
Keerthi sai sathwik <sai.k@samikhaswebservices.com>
2025-06-29 11:29:55 +0530

    updated the imports (Intellij Warning message)

--- src/main/java/com/example/journalApp/entity/JournalEntry.java
+++ src/main/java/com/example/journalApp/entity/JournalEntry.java
@@ -2,12 +2,10 @@ package com.example.journalApp.entity;
 
 import lombok.Data;
 import org.bson.types.ObjectId;
-import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import java.time.LocalDateTime;
-import java.util.Date;
 
 @Data
 @Document(collection = "journal_entries")
