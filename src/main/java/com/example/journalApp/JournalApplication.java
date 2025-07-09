de607948c070aad8cda27ce819a7527bd0082324
Keerthi sai sathwik <sai.k@samikhaswebservices.com>
2025-07-09 08:25:40 +0530

    @Transactional added. So @EnableTransacctionManagement is also needed in main file to use this annotation.
    Mongo shell URI added in application.properties but it is not working so added in main java file too.

--- src/main/java/com/example/journalApp/JournalApplication.java
+++ src/main/java/com/example/journalApp/JournalApplication.java
@@ -2,12 +2,27 @@ package com.example.journalApp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.data.mongodb.MongoDatabaseFactory;
+import org.springframework.data.mongodb.MongoTransactionManager;
+import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @SpringBootApplication
+@EnableTransactionManagement
 public class JournalApplication {
 
 	public static void main(String[] args) {
+		System.setProperty("spring.data.mongodb.uri",
+				"mongodb+srv://cluster:cluster12345@cluster0.kcdxtpw.mongodb.net/journaldb?retryWrites=true&w=majority&appName=Cluster0");
 		SpringApplication.run(JournalApplication.class, args);
 	}
 
+	@Bean
+	public PlatformTransactionManager add(MongoDatabaseFactory dbFactory){
+		return new MongoTransactionManager(dbFactory);
+	}
+
 }
+//If you want to make it clean, create a package "config" with a java file ex:TransactionalManagement class and annotate that java class with @Configuration.
+// you can write this @Bean function there and write the @EnableTransactionManagement below the @Configuration annotation right there it will work the same.
--- src/main/java/com/example/journalApp/service/JournalEntryService.java
+++ src/main/java/com/example/journalApp/service/JournalEntryService.java
@@ -7,6 +7,7 @@ import org.bson.types.ObjectId;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.time.LocalDateTime;
 import java.util.List;
@@ -22,11 +23,13 @@ public class JournalEntryService {
     @Autowired
     public UserService userService;
 
+    @Transactional
     public void saveEntryByUsername(JournalEntry journalEntry, String username){
         User user = userService.findByUsername(username);
         journalEntry.setDate(LocalDateTime.now());
         JournalEntry saved = journalEntryRepository.save(journalEntry);
         user.getJournalEntries().add(saved);
+//        user.setUsername(null);
         userService.saveEntry(user);
     }
 
